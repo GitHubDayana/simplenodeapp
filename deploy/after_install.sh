97ff5e95477d9b6043b8e3d983d6b963502d5b81
GitHubDayana <dayanareddy4199@gmail.com>
2024-11-01 12:26:30 +0530

    Update after_install.sh

--- deploy/after_install.sh
+++ deploy/after_install.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+# Install Node.js and npm if not already installed
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.nvm/nvm.sh
+nvm install node
+
+# Make npm globally accessible in this session
+export PATH=$PATH:/root/.nvm/versions/node/$(nvm version)/bin
+
+# Verify npm installation
+npm -v
+
 cd /home/ubuntu/simple-nodejs-app
 
 npm install
