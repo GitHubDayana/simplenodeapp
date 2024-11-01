5a93807441059a91f13b535795b385a48c026c3c
GitHubDayana <dayanareddy4199@gmail.com>
2024-11-01 12:32:57 +0530

    Update application_start.sh

--- deploy/application_start.sh
+++ deploy/application_start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Ensure Node.js and npm are installed (if they aren’t already from a previous step)
+curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
+sudo yum install -y nodejs
+
+# Install pm2 globally
+sudo npm install -g pm2
+
+# Verify pm2 installation
+pm2 -v
+
+
 cd /home/ubuntu/simple-nodejs-app
 pm2 delete server
 pm2 start server.js
